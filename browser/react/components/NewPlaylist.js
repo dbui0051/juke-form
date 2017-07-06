57eec0b87faca81dbf19f8c8e5304f6d392b8926
Kevin Genus <kevin.genus@gmail.com>
2017-07-06 18:41:38 -0400

    Validate Warnings pre inline render

--- browser/react/components/NewPlaylist.js
+++ browser/react/components/NewPlaylist.js
@@ -2,55 +2,62 @@ import React from 'react'
 
 export default class NewPlaylist extends React.Component {
 
-	constructor () {
-		super()
-		this.state = {
-			input: '',
-			validInput: true
-		}
-		this.handleChange = this.handleChange.bind(this)
-		this.handleSubmit = this.handleSubmit.bind(this)
-	}
+    constructor() {
+        super()
+        this.state = {
+            input: '',
+            validInput: true,
+            inputFormControl: 'form-control'
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
 
-	handleValidation () {
-		(this.state.input.length <= 16) && (this.state.input.length > 0) ? this.setState({validInput: false}) : this.setState({validInput: true})  
-	}
+    handleValidation() {
+        (this.state.input.length <= 16) && (this.state.input.length >= 0) ? this.setState({validInput: false}) : this.setState({validInput: true})
+    }
 
-	handleChange (event) {
-		this.handleValidation()
+    handleChange(event) {
+        this.setState({input: event.target.value});
+        if (this.handleValidation()) {
+            this.setState({inputFormControl: 'form-control alert alert-warning'});
+        } else {
+            this.setState({inputFormControl: 'form-control'});
+        }
 
-		this.setState({
-			input: event.target.value
-		})
-	}
+        console.log(this.state.inputFormControl);
+    }
 
-	handleSubmit (event) {
-		event.preventDefault()
-		this.setState({
-			input: ''
-		})
-	}
+    handleSubmit(event) {
+        event.preventDefault()
+        this.setState({
+            input: ''
+        })
+    }
 
-	render () {
-		return (
-			<div className="well">
-			  <form onSubmit={this.handleSubmit} className="form-horizontal">
-			    <fieldset>
-			      <legend>New Playlist</legend>
-			      <div className="form-group">
-			        <label className="col-xs-2 control-label">Name</label>
-			        <div className="col-xs-10">
-			          <input value={this.state.input} onChange={this.handleChange} className="form-control" type="text"/>
-			        </div>
-			      </div>
-			      <div className="form-group">
-			        <div className="col-xs-10 col-xs-offset-2">
-			          <button type="submit" className="btn btn-success" disabled={this.state.validInput}>Create Playlist</button>
-			        </div>
-			      </div>
-			    </fieldset>
-			  </form>
-			</div>
-		)
-	}
+    render() {
+        return (
+            <div className="well">
+                <form onSubmit={this.handleSubmit} className="form-horizontal">
+                    <fieldset>
+                        <legend>New Playlist</legend>
+                        <div className="form-group">
+                            <label className="col-xs-2 control-label">Name</label>
+                            <div className="col-xs-10">
+                                <input value={this.state.input} onChange={this.handleChange}
+                                       className={this.state.inputFormControl} type="text"/>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-xs-10 col-xs-offset-2">
+                                <button type="submit" className="btn btn-success" disabled={this.state.validInput}>
+                                    Create Playlist
+                                </button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        )
+    }
 }
